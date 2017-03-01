d98310a7263794839dddca9712c8fad159e356ca
Sid Ngeth <sngeth@gmail.com>
2017-03-01 15:59:38 -0500

    Add oauth2 serializers

--- config/config.exs
+++ config/config.exs
@@ -21,3 +21,10 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :oauth2,
+      serializers: %{
+        "application/vnd.collection+json" => Poison,
+        "application/vnd.api+json" => Poison,
+        "application/json" => Poison,
+      }
--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule Alexa.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Alexa, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext]]
+     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext, :oauth2]]
   end
 
   # Specifies which paths to compile per environment.
